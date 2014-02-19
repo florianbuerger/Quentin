d72197896ac4fedb4a4e9f3ce4fa8bd64c9a811e
Florian Buerger <flo@keslcod.com>
2014-02-19 08:42:42 +0100

    Initial Commit

---
+++ Quentin/QTNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QTNAppDelegate.h
+//  Quentin
+//
+//  Created by Florian Bürger on 19/02/14.
+//  Copyright (c) 2014 keslcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quentin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quentin
+//
+//  Created by Florian Bürger on 19/02/14.
+//  Copyright (c) 2014 keslcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QTNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QTNAppDelegate class]));
+    }
+}
